407c0c44cdaeb7c5ed801b6a119af47af9e2e3fb
Mandar Nilange <mandarnilange@gmail.com>
2025-10-19 22:01:26 +0530

    feat: add SummaryStatistics entity

---
+++ lib/domain/entities/biomarker_trend_summary.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class BiomarkerTrendSummary extends Equatable {
+  const BiomarkerTrendSummary(); // Empty for now
+
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/domain/entities/vital_trend_summary.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class VitalTrendSummary extends Equatable {
+  const VitalTrendSummary(); // Empty for now
+
+  @override
+  List<Object?> get props => [];
+}
