6bfb0d724710e077953b91fd5083258f37c01c11
Mandar Nilange <mandarnilange@gmail.com>
2025-10-20 06:07:49 +0530

    test: add ChartRenderingService tests

--- lib/data/datasources/external/chart_rendering_service.dart
+++ lib/data/datasources/external/chart_rendering_service.dart
@@ -18,41 +18,54 @@ class ChartRenderingServiceImpl implements ChartRenderingService {
   @override
   Widget getLineChart(List<Biomarker> biomarkers, ReferenceRange? range, TrendAnalysis? trend) {
     final spots = biomarkers.asMap().entries.map((e) => FlSpot(e.key.toDouble(), e.value.value)).toList();
+    final List<LineChartBarData> lineBarsData = [
+      LineChartBarData(
+        spots: spots,
+        dotData: FlDotData(
+          show: true,
+          getDotPainter: (spot, percent, barData, index) {
+            final biomarker = biomarkers[index];
+            Color color;
+            switch (biomarker.status) {
+              case BiomarkerStatus.high:
+                color = Colors.red;
+                break;
+              case BiomarkerStatus.low:
+                color = Colors.orange;
+                break;
+              default:
+                color = Colors.green;
+                break;
+            }
+            return FlDotCirclePainter(radius: 4, color: color);
+          },
+        ),
+      ),
+    ];
+
+    if (range != null) {
+      final maxX = (spots.length - 1).toDouble();
+      lineBarsData.add(LineChartBarData(
+        spots: [FlSpot(0, range.min), FlSpot(maxX, range.min)],
+        dotData: FlDotData(show: false),
+        barWidth: 0,
+      ));
+      lineBarsData.add(LineChartBarData(
+        spots: [FlSpot(0, range.max), FlSpot(maxX, range.max)],
+        dotData: FlDotData(show: false),
+        barWidth: 0,
+      ));
+    }
 
     return Stack(
       children: [
         LineChart(
           LineChartData(
-            lineBarsData: [
-              LineChartBarData(
-                spots: spots,
-                dotData: FlDotData(
-                  show: true,
-                  getDotPainter: (spot, percent, barData, index) {
-                    final biomarker = biomarkers[index];
-                    Color color;
-                    switch (biomarker.status) {
-                      case BiomarkerStatus.high:
-                        color = Colors.red;
-                        break;
-                      case BiomarkerStatus.low:
-                        color = Colors.orange;
-                        break;
-                      default:
-                        color = Colors.green;
-                        break;
-                    }
-                    return FlDotCirclePainter(radius: 4, color: color);
-                  },
-                ),
-              ),
-            ],
+            lineBarsData: lineBarsData,
             betweenBarsData: range != null ? [
               BetweenBarsData(
-                fromIndex: 0,
-                toIndex: 0,
-                fromY: range.min,
-                toY: range.max,
+                fromIndex: 1,
+                toIndex: 2,
                 color: Colors.green.withOpacity(0.2),
               )
             ] : [],
